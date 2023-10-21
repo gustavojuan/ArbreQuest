479a03f7f74fb0a353d2fcff0d1e020fad57f2a2
gustavojuan <gustavojuan@gmail.com>
2023-10-21 08:15:17 +0200

    [FEAT] Añadido Tailwind y probada navegacion básicaa

--- about.view.php
+++ about.view.php
@@ -26,7 +26,7 @@
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                             <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Inicio</a>
-                            <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Sobre nosotros</a>
+                            <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Sobre nosotros</a>
 
 
                         </div>
@@ -134,7 +134,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>Contenido pirnicpal</p>
+            <p>Contenido ABOUT</p>
         </div>
     </main>
 
