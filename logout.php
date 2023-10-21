abf19e498f60bd4e4ab168b2d3c223bb178154c5
gustavojuan <gustavojuan@gmail.com>
2023-10-21 17:11:12 +0200

    [TYPO] ERrores comentarios

--- logout.php
+++ logout.php
@@ -1,11 +1,9 @@
 <?php
-// Inicia la sesión
-session_start();
 
-// Destruye la sesión
+session_start();
 session_destroy();
 
-// Redirige al usuario a la página de inicio o a donde desees
-header('Location: /'); // Puedes cambiar la URL según tus necesidades
+
+header('Location: /');
 exit();
 ?>
