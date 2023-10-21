e15a34eb52873aa6268c6164f41c40809b65e119
gustavojuan <gustavojuan@gmail.com>
2023-10-21 07:51:42 +0200

    [REFACT] Filtrado By Municpio por Array_FILTER

--- index.php
+++ index.php
@@ -4,37 +4,45 @@ $trees = [
     [
         "id" => 1,
         "titulo" => "Pino Gigante del Parque de Cabecera",
-        "descripcion" => "El Pino Gigante del Parque de Cabecera es un impresionante ejemplar que se alza majestuoso en el corazón de Valencia. Con más de 200 años de edad, este árbol es un verdadero monumento a la naturaleza.",
+        "descripcion" => "El Pino Gigante del Parque de Cabecera es un impresionante ejemplar que se alza majestuoso en el corazón de Valencia. Con más de 200 años de edad, este pino pertenece a la familia de los Pinaceae.",
         "imagen" => "https://source.unsplash.com/800x600/?tree",
         "ubicacion" => [
             "latitud" => 39.4699,
             "longitud" => -0.3774
         ],
-        "municipio" => "Valencia"
+        "municipio" => "Valencia",
+        "especie" => "Pinus"
     ],
     [
         "id" => 2,
         "titulo" => "Olivo Centenario de Gandía",
-        "descripcion" => "El Olivo Centenario de Gandía es un símbolo de resistencia y longevidad. Este antiguo olivo ha presenciado siglos de historia y es un verdadero tesoro vivo en la ciudad de Gandía.",
+        "descripcion" => "El Olivo Centenario de Gandía es un símbolo de resistencia y longevidad. Este antiguo olivo es de la especie Olea europaea.",
         "imagen" => "https://source.unsplash.com/800x600/?nature",
         "ubicacion" => [
             "latitud" => 39.4683,
             "longitud" => -0.3744
         ],
-        "municipio" => "Gandía"
+        "municipio" => "Gandía",
+        "especie" => "Olea europaea"
     ],
     [
         "id" => 3,
         "titulo" => "Roble Milenario de Alzira",
-        "descripcion" => "El Roble Milenario de Alzira es una maravilla natural que ha sobrevivido al paso del tiempo. Con más de 1,000 años de antigüedad, este roble es un testigo de la historia de Alzira y sus alrededores.",
+        "descripcion" => "El Roble Milenario de Alzira es un impresionante ejemplar de roble de la especie Quercus robur, con más de 1,000 años de antigüedad.",
         "imagen" => "https://source.unsplash.com/800x600/?forest",
         "ubicacion" => [
             "latitud" => 39.4700,
             "longitud" => -0.3770
         ],
-        "municipio" => "Alzira"
+        "municipio" => "Alzira",
+        "especie" => "Quercus robur"
     ]
 ];
+
+
+$filteredTrees = array_filter($trees,function ($tree){
+    return $tree['municipio'] === 'Gandía';
+});
 
 
 ?>
@@ -50,18 +58,32 @@ $trees = [
 </head>
 <body>
 
-<ul>
-    <?php foreach ($trees
 
-    as $tree) : ?>
+
+
+<ul>
+    <?php foreach ($trees    as $tree) : ?>
     <li>
         <a href="/tree/<?= $tree['id'] ?>">
-            <?= $tree['titulo']; ?> (<?= $tree['municipio'] ?>)?>
+            <?= $tree['titulo']; ?> (<?= $tree['municipio'] ?>)
         </a>
     </li>
+
+    <?php endforeach; ?>
 </ul>
 
-<?php endforeach; ?>
+<ul>
+    <?php foreach ($filteredTrees   as $tree) : ?>
+    <li>
+        <a href="/tree/<?= $tree['id'] ?>">
+            <?= $tree['titulo']; ?> (<?= $tree['municipio'] ?>)
+        </a>
+    </li>
+
+    <?php endforeach; ?>
+</ul>
+
+
 
 </body>
 </html>
